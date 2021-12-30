622ac3719938801ece52fd7c5e32ec6dbc23381c
Randy W. Sims <simsrw73@gmail.com>
2021-12-30 17:26:26 -0500

    replace links with files :-/

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,12 @@
-C:/Users/simsr/OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+Import-Module oh-my-posh
+Import-Module posh-git
+Import-Module Terminal-Icons
+Import-Module PSReadLine
+
+$env:POSH_GIT_ENABLED = $true
+
+Set-PoshPrompt ~/simple.omp.json
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
--- oh-my-posh.lua
+++ oh-my-posh.lua
@@ -1,1 +1,1 @@
-C:/Users/simsr/AppData/Local/clink/oh-my-posh.lua
+load(io.popen('"C:/Users/simsr/OneDrive/Documents/PowerShell/Modules/oh-my-posh/oh-my-posh.EXE" --config=C:/Users/simsr/simple.omp.json --init --shell cmd'):read("*a"))()
