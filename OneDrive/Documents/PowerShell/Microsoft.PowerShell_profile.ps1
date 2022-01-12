80133ee258d2cb3d3107a03e90fbb0a4b689ccd1
Randy W. Sims <simsrw73@gmail.com>
2022-01-12 16:55:13 -0500

    Use dotfile manager: Chezmoi

--- OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ Import-Module posh-git
 Import-Module Terminal-Icons
 Import-Module PSReadLine
 
+# $env:PAGER = 'less'
 $env:POSH_GIT_ENABLED = $true
 
 Set-PoshPrompt ~/simple.omp.json
@@ -10,3 +11,5 @@ Set-PoshPrompt ~/simple.omp.json
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
+
+Remove-Alias -Name r -Force -ErrorAction SilentlyContinue
